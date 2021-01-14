9375ba71b53de62e933d095f24a2bf033c7e9618
hiro <wfy11235813@gmail.com>
2021-01-14 15:49:51 +0800

    new node map: 12 nodes, 4 faulty nodes, 8 normal nodes

--- analysis/draw_nodes.py
+++ analysis/draw_nodes.py
@@ -1,3 +1,9 @@
+import sys
+
+sys.path.append("/home/hiro/Documents/resilient-consensus/")
+print(sys.path)
+
+
 import matplotlib
 import matplotlib.pyplot as plt
 import networkx as nx
@@ -13,21 +19,23 @@ def concat_edges(env: Env):
     return edges
 
 
-# env = Env(nodes_n=10, evil_nodes_type='3r')
-env = CarEnv()
+env = Env(nodes_n=12, evil_nodes_type="4r", directed_graph=True)
+# env = CarEnv()
 
-matplotlib.use('Agg')
+matplotlib.use("Agg")
 G = nx.DiGraph()
 G.add_nodes_from([x for x in range(env.nodes_n)])
 G.add_edges_from(concat_edges(env))
-nx.draw(G,
-        pos=nx.circular_layout(G),
-        with_labels=True,
-        # edge_color='b',
-        node_color=['#99A3A4' for _ in range(env.nodes_n-env.goods_n)] + ['green' for _ in range(env.goods_n)],
-        # arrowstyle='-',
-        node_size=1500,
-        font_size=14
-        )
+nx.draw(
+    G,
+    pos=nx.circular_layout(G),
+    with_labels=True,
+    # edge_color='b',
+    node_color=["#99A3A4" for _ in range(env.nodes_n - env.goods_n)]
+    + ["green" for _ in range(env.goods_n)],
+    # arrowstyle='-',
+    node_size=1500,
+    font_size=14,
+)
 plt.show()
-plt.savefig('nodes.eps', format='eps')
+plt.savefig("nodes.eps", format="eps")
